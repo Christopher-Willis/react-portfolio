63a7186e51c8146f31fd76814e0e5ad0468f4f5b
Christopher Willis <willis.christopher.a@gmail.com>
2018-12-11 15:06:33 -0600

    Landing page section added, formated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,86 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 import Portfolioitems from './components/portfolioitems/Portfoiloitem';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+
+
 import './App.css';
+
+library.add(fas,fab);
+
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <Portfolioitems title="This Webpage :D">
+      <Grid container spacing={24} alignItems="center" justify="center" style={{height:600}}>
+        <Grid item xs={20}>
+          <h1>
+            Hello, I'm Chistopher!
+          </h1>
+          <h3>
+            |Full Stack Dev  ||  SEO Monkey  ||  Clean Energy Buff  ||  Addicted to Python| 
+          </h3>
+          <IconButton style={{padding:3}} size="large"  href="https://github.com/BeCurieUs/" >
+            <FontAwesomeIcon icon={['fab', 'github']} size="2x" color="white"/> 
+          </IconButton>
+          <IconButton style={{padding:3}} size="large" href="https://twitter.com/BeCurieus" >
+            <FontAwesomeIcon icon={['fab', 'twitter']} size="2x" color="white" /> 
+          </IconButton>
+          <IconButton style={{padding:3}} size="large" href="https://www.linkedin.com/in/christopher-willis-8a399a151/" >
+            <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" color="white" /> 
+          </IconButton>
+        </Grid>
+      </Grid>
+
+
+      
+      <Grid container spacing={24} justify="space-evenly" >
+        <Grid item xs={6}>
+          <h2>
+            Portfolio Projects:
+          </h2>
+        </Grid>
+        <Grid item xs={6}>
+        </Grid>
+        <Grid item >
+          <Portfolioitems 
+            title="This Webpage :D" 
+            content = "Look no further if you want the source code for this page">
           </Portfolioitems>
-        </header>
+        </Grid>
+        <Grid item >
+          <Portfolioitems 
+            title="This Webpage :D" 
+            content = "Look no further if you want the source code for this page">
+          </Portfolioitems>
+        </Grid>
+        <Grid item >
+          <Portfolioitems 
+            title="This Webpage :D" 
+            content = "Look no further if you want the source code for this page">
+          </Portfolioitems>
+        </Grid>
+      </Grid>
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+// <Portfolioitems 
+// title="This Webpage :D" 
+// content = "Look no further if you want the source code for this page">
+// </Portfolioitems>
--- src/components/portfolioitems/Portfoiloitem.js
+++ src/components/portfolioitems/Portfoiloitem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+
 import CardContent from '@material-ui/core/CardContent';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import Tooltip from '@material-ui/core/Tooltip';
+import CardActions from '@material-ui/core/CardActions';
+
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 
 
@@ -17,12 +25,27 @@ class Portfoiloitem extends Component {
     return (
       <Card style={{maxWidth: 215,margin:8}}>
         <CardHeader title= {this.props.title}/>
+        <Divider />
         <CardContent>
-          <Typography>
-            Look no further if you want the source code for this page
+          <Typography gutterBottom component="p">
+            {this.props.content}
           </Typography>
-          <FontAwesomeIcon></FontAwesomeIcon>
         </CardContent>
+        <CardContent>
+          <Typography variant="caption" >made with:</Typography>
+          <Tooltip title="Javascript" >
+            <FontAwesomeIcon  style ={{marginTop:1}}  icon={['fab', 'js']} color="black" size="lg"/>
+          </Tooltip>
+          <Tooltip title="React" >
+            <FontAwesomeIcon icon={['fab', 'react']} color="black" size="lg"/>
+          </Tooltip>
+        </CardContent>
+        <Divider />
+        <CardActions>
+          <Button size="small" href="https://github.com/BeCurieUs/react-portfolio" >See on GitHub 
+            <FontAwesomeIcon style={{marginLeft:5}} icon={['fab', 'github']} color="black" size="lg"/> 
+          </Button>
+        </CardActions>
       </Card>
     );
   }
